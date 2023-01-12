c7499ebb9c18b27855e9a35affb8face354f33fb
Massimo <37666636+Supergiovane@users.noreply.github.com>
2023-01-12 12:47:43 +0100

    .

--- nodes/utils/hikDiscovery.js
+++ nodes/utils/hikDiscovery.js
@@ -21,55 +21,60 @@ exports.Discover = async function getAllHikvisionDevicesDescriptors() {
         } catch (error) {
 
         }
-        const dgram = require('dgram');
-        const client = dgram.createSocket({ type: 'udp4', reuseAddr: true });
+        try {
+            const dgram = require('dgram');
+            const client = dgram.createSocket({ type: 'udp4', reuseAddr: true });
 
-        client.on('listening', function () {
-            let currentAddress = client.address();
-            //console.log('UDP Client listening on ' + currentAddress.address + ":" + currentAddress.port);
-            client.setBroadcast(true)
-            client.setMulticastTTL(128);
-            client.addMembership(MCAST_ADDR);
-        });
+            client.on('listening', function () {
+                let currentAddress = client.address();
+                //console.log('UDP Client listening on ' + currentAddress.address + ":" + currentAddress.port);
+                client.setBroadcast(true)
+                client.setMulticastTTL(128);
+                client.addMembership(MCAST_ADDR);
+            });
 
-        client.on('message', function (message, remote) {
+            client.on('message', function (message, remote) {
 
-            try {
-                
-                let jObj = parser.parse(message.toString());
-                if (jObj.hasOwnProperty("ProbeMatch")) {
-                    if (aDevices.find(a => a.IPv4Address === jObj.ProbeMatch.IPv4Address) === undefined) {
-                        aDevices.push(jObj.ProbeMatch);  
-                    } 
+                try {
+
+                    let jObj = parser.parse(message.toString());
+                    if (jObj.hasOwnProperty("ProbeMatch")) {
+                        if (aDevices.find(a => a.IPv4Address === jObj.ProbeMatch.IPv4Address) === undefined) {
+                            aDevices.push(jObj.ProbeMatch);
+                        }
+                    }
+
+                } catch (error) {
+                    reject([]);
+                    console.log('getAllHikvisionDevicesDescriptors: From: ' + remote.address + ':' + remote.port + ' - ' + error.message);
                 }
 
-            } catch (error) {
-                reject([]);
-                console.log('getAllHikvisionDevicesDescriptors: From: ' + remote.address + ':' + remote.port + ' - ' + error.message);
-            }
-
-        });
-
-        client.bind(MCAST_PORT);
-
-        // Send query message
-        setTimeout(() => {
-            let b = Buffer.from(discoMSG);
-            client.send(b, 0, b.length, MCAST_PORT, MCAST_ADDR, function () {
-                //console.log("Sent " + b);
             });
-        }, 10);
 
-        // Set timeout timer
-        setTimeout(() => {
-            try {
-                client.dropMembership(MCAST_ADDR);
-                client.close();
-                client.disconnect();
-            } catch (error) {
-            }
-            resolve(aDevices);
-        }, 2000);
+            client.bind(MCAST_PORT);
+
+            // Send query message
+            setTimeout(() => {
+                let b = Buffer.from(discoMSG);
+                client.send(b, 0, b.length, MCAST_PORT, MCAST_ADDR, function () {
+                    //console.log("Sent " + b);
+                });
+            }, 10);
+
+            // Set timeout timer
+            setTimeout(() => {
+                try {
+                    client.dropMembership(MCAST_ADDR);
+                    client.close();
+                    client.disconnect();
+                } catch (error) {
+                }
+                resolve(aDevices);
+            }, 2000);
+        } catch (error) {
+
+        }
+
     })
 
 }
